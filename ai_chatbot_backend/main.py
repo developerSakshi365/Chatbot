d3082532b1b2b791d7b2a14ccf0a09960edd4490
developerSakshi365 <developersakshi365@gmail.com>
2026-02-11 16:27:33 +0530

    add cors

--- ai_chatbot_backend/main.py
+++ ai_chatbot_backend/main.py
@@ -20,13 +20,13 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "http://127.0.0.1:3000"
+        "http://127.0.0.1:3000",
+        "https://chatbot-git-main-sakshis-projects-110cae2b.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 
 # ---------- REQUEST MODELS ----------
 
