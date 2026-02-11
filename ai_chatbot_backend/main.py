a74c684ad576e8a1a91c0e53fa7209739e413568
developerSakshi365 <developersakshi365@gmail.com>
2026-02-11 16:42:33 +0530

    add production vercel url

--- ai_chatbot_backend/main.py
+++ ai_chatbot_backend/main.py
@@ -21,7 +21,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
-        "https://chatbot-git-main-sakshis-projects-110cae2b.vercel.app"
+        "https://chatbot-woad-one.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
